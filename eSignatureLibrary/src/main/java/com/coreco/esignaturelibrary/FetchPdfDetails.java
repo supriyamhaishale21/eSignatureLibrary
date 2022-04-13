72a6f6728893f305f2de3fe6ecda6d5786d3dfb5
supriyamhaishale21 <supriya.mhaishale@corecotechnologies.com>
2022-04-13 11:19:20 +0530

    Added these details.

---
+++ eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
@@ -0,0 +1,12 @@
+package com.coreco.esignaturelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class FetchPdfDetails {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
