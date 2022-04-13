12c075cf8cd40c71c98402b94dd56213b61b758e
supriyamhaishale21 <supriya.mhaishale@corecotechnologies.com>
2022-04-13 15:55:11 +0530

    Updated files.

--- eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
+++ eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
@@ -3,6 +3,7 @@ package com.coreco.esignaturelibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -33,29 +34,70 @@ public class FetchPdfDetails {
         }
         Document document = documentBuilder.newDocument();
         Element rootElement = document.createElement("Esign");
-        rootElement.getElementsByTagNameNS("ver=","2.1");
+        // set an attribute to staff element
+        Attr attr = document.createAttribute("ver");
+        attr.setValue("2.1");
+        rootElement.setAttributeNode(attr);
 
-        rootElement.setTextContent("'ver'= \"'2.1'\"");
-        rootElement.setTextContent("'AuthMode'= \"'1'\"");
-        rootElement.setTextContent("'aspId'= \"'VTL001'\"");
-        rootElement.setTextContent("'ekycIdType'= \"'A'\"");
-        rootElement.setTextContent("'responseSigType'= \"'pkcs7pdf'\"");
-        rootElement.setTextContent("'responseUrl'= \"\"'http://127.0.0.1:7080/aspesignresponse'\"");
-        rootElement.setTextContent("'sc'= \"'Y'\"");
-        rootElement.setTextContent("'ts'= \"'2021-01-14T17:35:39.891+05:3'\"");
-        rootElement.setTextContent("'txn'= \"'VTL001:14012021053537000208:1987'\"");
+        Attr attr1 = document.createAttribute("AuthMode");
+        attr1.setValue("1");
+        rootElement.setAttributeNode(attr1);
+
+        Attr attr2 = document.createAttribute("aspId");
+        attr2.setValue("VTL001");
+        rootElement.setAttributeNode(attr2);
+
+        Attr attr3 = document.createAttribute("ekycIdType");
+        attr3.setValue("A");
+        rootElement.setAttributeNode(attr3);
+
+        Attr attr4 = document.createAttribute("responseSigType");
+        attr4.setValue("pkcs7pdf");
+        rootElement.setAttributeNode(attr4);
+
+        Attr attr5 = document.createAttribute("responseUrl");
+        attr5.setValue("http://127.0.0.1:7080/aspesignresponse");
+        rootElement.setAttributeNode(attr5);
+
+        Attr attr6 = document.createAttribute("sc");
+        attr6.setValue("Y");
+        rootElement.setAttributeNode(attr6);
+
+        Attr attr7 = document.createAttribute("ts");
+        attr7.setValue("2021-01-14T17:35:39.891+05:3");
+        rootElement.setAttributeNode(attr7);
+
+        Attr attr8 = document.createAttribute("txn");
+        attr8.setValue("VTL001:14012021053537000208:1987");
+        rootElement.setAttributeNode(attr8);
+
         document.appendChild(rootElement);
 
         Element doc_em = document.createElement("Docs");
         rootElement.appendChild(doc_em);
+
         Element inputhash_em = document.createElement("InputHash");
-        inputhash_em.setTextContent("'docInfo=' \"'eSigning of PDF Document'\"");
-        inputhash_em.setTextContent("'hashAlgorithm=' \"'SHA256'\"");
-        inputhash_em.setTextContent("'id=' \"'1'\"");
         doc_em.appendChild(inputhash_em);
-        inputhash_em.setTextContent("a20a9d837daf39efb889e8246031b6e315e5c7a0793394a4a4bf6a3342b75109");
+
+        Attr input_attr = document.createAttribute("docInfo");
+        input_attr.setValue("eSigning of PDF Document");
+        inputhash_em.setAttributeNode(input_attr);
+
+        Attr input_attr1 = document.createAttribute("hashAlgorithm");
+        input_attr1.setValue("SHA256");
+        inputhash_em.setAttributeNode(input_attr1);
+
+        Attr input_attr2 = document.createAttribute("id");
+        input_attr2.setValue("1");
+        inputhash_em.setAttributeNode(input_attr2);
+
+        inputhash_em.appendChild(document.createTextNode("a20a9d837daf39efb889e8246031b6e315e5c7a0793394a4a4bf6a3342b75109"));
+
         Element signature_em = document.createElement("Signature");
-        signature_em.setTextContent("xmlns= \"http://www.w3.org/2000/09/xmldsig#\"");
+        Attr signature_attr = document.createAttribute("");
+        signature_attr.setValue("xmlns=http://www.w3.org/2000/09/xmldsig#");
+        signature_em.setAttributeNode(signature_attr);
+
         rootElement.appendChild(signature_em);
 
         TransformerFactory transformerFactory = TransformerFactory.newInstance();  // This code
