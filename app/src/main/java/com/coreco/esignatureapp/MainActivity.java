4cd103332922a69e98178bd1c7f61a3f80cde64b
supriyamhaishale21 <supriya.mhaishale@corecotechnologies.com>
2022-04-13 13:26:35 +0530

    Updated  files.

--- app/src/main/java/com/coreco/esignatureapp/MainActivity.java
+++ app/src/main/java/com/coreco/esignatureapp/MainActivity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 
 import com.coreco.esignaturelibrary.FetchPdfDetails;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> pdf_details=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FetchPdfDetails.s(this,"Library included successfully.");
+        pdf_details.add("200");
+
+        FetchPdfDetails.fetchDetails(this,pdf_details);
     }
 }
--- eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
+++ eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
@@ -3,14 +3,59 @@ package com.coreco.esignaturelibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import java.util.ArrayList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 public class FetchPdfDetails {
-    public static void fetchDetails(Context c, ArrayList<String> pdfDetails){
+    public static void fetchDetails(Context c, ArrayList<String> pdfDetails) {
 
-        Toast.makeText(c,"Library added successfully.",Toast.LENGTH_SHORT).show();
-        for (int i = 0; i <pdfDetails.size() ; i++) {
 
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = null;
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document document = documentBuilder.newDocument();
+        Element rootElement = document.createElement("map");
+        document.appendChild(rootElement);
+
+        Element em = document.createElement("string");
+        em.setAttribute("name", "FirstName");
+        em.appendChild(document.createTextNode("Rita"));
+        rootElement.appendChild(em);
+
+        em = document.createElement("string");
+        em.setAttribute("name", "LastName");
+        em.appendChild(document.createTextNode("Roy"));
+        rootElement.appendChild(em);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();  // This code
+        Transformer transformer = null;                // doesn't work
+        try {
+            transformer = transformerFactory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+        DOMSource source = new DOMSource(document);                                  // with Android
+        StreamResult result = new StreamResult("c:\\abc.xml");                          //  :(
+        try {
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            e.printStackTrace();
         }
 
     }
