99b9741eeedbf1f57eb82c1a78e038e745c198f9
supriyamhaishale21 <supriya.mhaishale@corecotechnologies.com>
2022-04-13 12:42:27 +0530

    Updated method files.

--- eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
+++ eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
@@ -3,10 +3,15 @@ package com.coreco.esignaturelibrary;
 import android.content.Context;
 import android.widget.Toast;
 
-public class FetchPdfDetails {
-    public static void s(Context c, String message){
+import java.util.ArrayList;
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+public class FetchPdfDetails {
+    public static void fetchDetails(Context c, ArrayList<String> pdfDetails){
+
+        Toast.makeText(c,"Library added successfully.",Toast.LENGTH_SHORT).show();
+        for (int i = 0; i <pdfDetails.size() ; i++) {
+
+        }
 
     }
 }
