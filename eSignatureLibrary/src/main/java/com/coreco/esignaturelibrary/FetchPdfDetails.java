8a84c5ab053db87281ecfdc2b6f3efbc2817de72
supriyamhaishale21 <supriya.mhaishale@corecotechnologies.com>
2022-04-13 14:46:23 +0530

    Updated file.

--- eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
+++ eSignatureLibrary/src/main/java/com/coreco/esignaturelibrary/FetchPdfDetails.java
@@ -33,24 +33,25 @@ public class FetchPdfDetails {
         }
         Document document = documentBuilder.newDocument();
         Element rootElement = document.createElement("Esign");
+        rootElement.getElementsByTagNameNS("ver=","2.1");
 
-        rootElement.setTextContent("ver= \"2.1\"");
-        rootElement.setTextContent("AuthMode= \"1\"");
-        rootElement.setTextContent("aspId= \"VTL001\"");
-        rootElement.setTextContent("ekycIdType= \"A\"");
-        rootElement.setTextContent("responseSigType= \"pkcs7pdf\"");
-        rootElement.setTextContent("responseUrl= \"\"http://127.0.0.1:7080/aspesignresponse\"");
-        rootElement.setTextContent("sc= \"Y\"");
-        rootElement.setTextContent("ts= \"2021-01-14T17:35:39.891+05:3\"");
-        rootElement.setTextContent("txn= \"VTL001:14012021053537000208:1987\"");
+        rootElement.setTextContent("'ver'= \"'2.1'\"");
+        rootElement.setTextContent("'AuthMode'= \"'1'\"");
+        rootElement.setTextContent("'aspId'= \"'VTL001'\"");
+        rootElement.setTextContent("'ekycIdType'= \"'A'\"");
+        rootElement.setTextContent("'responseSigType'= \"'pkcs7pdf'\"");
+        rootElement.setTextContent("'responseUrl'= \"\"'http://127.0.0.1:7080/aspesignresponse'\"");
+        rootElement.setTextContent("'sc'= \"'Y'\"");
+        rootElement.setTextContent("'ts'= \"'2021-01-14T17:35:39.891+05:3'\"");
+        rootElement.setTextContent("'txn'= \"'VTL001:14012021053537000208:1987'\"");
         document.appendChild(rootElement);
 
         Element doc_em = document.createElement("Docs");
         rootElement.appendChild(doc_em);
         Element inputhash_em = document.createElement("InputHash");
-        inputhash_em.setTextContent("docInfo= \"eSigning of PDF Document\"");
-        inputhash_em.setTextContent("hashAlgorithm= \"SHA256\"");
-        inputhash_em.setTextContent("id= \"1\"");
+        inputhash_em.setTextContent("'docInfo=' \"'eSigning of PDF Document'\"");
+        inputhash_em.setTextContent("'hashAlgorithm=' \"'SHA256'\"");
+        inputhash_em.setTextContent("'id=' \"'1'\"");
         doc_em.appendChild(inputhash_em);
         inputhash_em.setTextContent("a20a9d837daf39efb889e8246031b6e315e5c7a0793394a4a4bf6a3342b75109");
         Element signature_em = document.createElement("Signature");
