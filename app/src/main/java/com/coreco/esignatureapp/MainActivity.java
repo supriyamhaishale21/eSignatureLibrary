4329168ea32027893d08b4fdb79534e7f66bd620
supriyamhaishale21 <supriya.mhaishale@corecotechnologies.com>
2022-04-15 12:24:33 +0530

    Updated gradle files.

--- app/src/main/java/com/coreco/esignatureapp/MainActivity.java
+++ app/src/main/java/com/coreco/esignatureapp/MainActivity.java
@@ -24,24 +24,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         pdf_details.add("200");
 
-        AppendDigitalSignOnPdf.getPDFeSignParams(this,"getTxnRefreq.json");
-    }
-    public String getJsonFromAssets(Context context, String fileName) {
-        String jsonString;
-        try {
-            InputStream is = context.getAssets().open(fileName);
-
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-            jsonString = new String(buffer, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-
-        return jsonString;
+        AppendDigitalSignOnPdf.GetPDFeSignParams(this,"getTxnRefreq.json");
     }
 }
